989175152e82d99513f480daf103a656b5b54ee6
Roque Leal <32916827+roqueleal@users.noreply.github.com>
2019-05-13 13:26:11 -0500

    dd
    dd

--- config.py
+++ config.py
@@ -6,5 +6,5 @@
 
 consumer_key = "NGvWSELEqFhb8kzsgoyRrQ2iS"
 consumer_secret = "efrfTopXB02DrQCAcuWwbC4ZFE0I6ObpwxhVbU1Rr60SEjBYho"
-access_key = "1062422137942761473-f1d8RY9zfC9BdzDYN5ag2lXf23vasw"
-access_secret = "PfjmJKUVbhUr59MhhPwebHXmGMRapoaHyaYh0MJoyvdT6"
+access_key = "1110175646511235079-QQAXZXbjwoEmd2di0dqJmNqvt008VE"
+access_secret = "Ni5VRkP33FCKsNJBmoXO62Wzv7xXZoHcelXIKMS4HEk0M"
