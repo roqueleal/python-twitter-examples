83a4e419962f6fda1a16e06e4c9fe5af6d6068b9
Roque Leal <32916827+roqueleal@users.noreply.github.com>
2019-05-13 10:55:36 -0500

    f
    f

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "XxXxXxxXXXxxxxXXXxXX"
-consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
-access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
-access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
+consumer_key = "NGvWSELEqFhb8kzsgoyRrQ2iS"
+consumer_secret = "efrfTopXB02DrQCAcuWwbC4ZFE0I6ObpwxhVbU1Rr60SEjBYho"
+access_key = "1062422137942761473-f1d8RY9zfC9BdzDYN5ag2lXf23vasw"
+access_secret = "PfjmJKUVbhUr59MhhPwebHXmGMRapoaHyaYh0MJoyvdT6"
